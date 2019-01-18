f97d2b477d4086b010f158fc114933d321a18bce
MrAlvin <mralvin@3iii.dk>
2019-01-18 01:53:16 +0100

    TMC2130 testing via Arduino library by teemuatlut

---
+++ Arduino test sketches/test steppers/TMC2130-using-library/TMC2130-using-library.ino
@@ -0,0 +1,110 @@
+/**
+ * This sketch will (with default settings) test stepper Y on a RAMPS 1.7 board, 
+ * by turning the stepper in alternating directions.
+ *
+ * You need the latest TMC2130 library from: https://github.com/teemuatlut/TMC2130Stepper
+ * 
+ * This sketch by MrAlvin, Jan 2019
+ * Original sketch and library by Teemu Mäntykallio 
+ * 
+ * 
+ * Change the TEST_STEPPER value, to test other stepper positions on the RAMPS 1.7 board
+ */
+
+#define TEST_STEPPER  2  // values can be 1 to 5 where: 1 => X, 2 => Y, 3 => Z, 4 => E0, 5 => E1
+
+/** 
+ *  You need the latest TMC2130 library from: https://github.com/teemuatlut/TMC2130Stepper
+ *  
+ ***  Library Installation Guide ***
+ *  
+ *  Option A) 
+ *       Use the Arduino IDE library manager (Sketch -> Include library -> Manage libraries...)
+ *       Search for TMC2130Stepper 
+ *       and then install.
+ *       
+ *  Option B)
+ *       Download the zip file from Github ( https://github.com/teemuatlut/TMC2130Stepper )
+ *       and extract it to: <arduino folder>/libraries
+ *       and restart the IDE.
+ *       
+ *  Option C)
+ *       goto to you arduino/libraries folder 
+ *       and in command line: git clone https://github.com/teemuatlut/TMC2130Stepper.git
+ */
+
+#if TEST_STEPPER == 2  // Y
+  //         Ramps17-Y    // examples
+  #define EN_PIN    58    // 38  // Nano v3:	16 Mega:	38	//enable (CFG6)
+  #define DIR_PIN   63    // 55  //			19			55	//direction
+  #define STEP_PIN  62    // 54  //			18			54	//step
+  #define CS_PIN    42    // 40  //			17			64	//chip select
+  char Step = 'Y';
+#elif TEST_STEPPER == 1  // X
+  #define EN_PIN    55  
+  #define DIR_PIN   57  
+  #define STEP_PIN  56  
+  #define CS_PIN    46  
+  char Step = 'X';
+#elif TEST_STEPPER == 3  // Z
+  #define EN_PIN    67  
+  #define DIR_PIN   69  
+  #define STEP_PIN  68  
+  #define CS_PIN    48
+  char Step = 'Z';
+#elif TEST_STEPPER == 4  // E0
+  #define EN_PIN    30  
+  #define DIR_PIN   36  
+  #define STEP_PIN  34  
+  #define CS_PIN    38
+  char Step = '0';
+#elif TEST_STEPPER == 5  // E1 
+  #define EN_PIN    22  
+  #define DIR_PIN   26  
+  #define STEP_PIN  24  
+  #define CS_PIN     6
+  char Step = '1';
+#endif
+
+
+
+bool dir = true;
+
+#include <TMC2130Stepper.h>
+TMC2130Stepper driver = TMC2130Stepper(EN_PIN, DIR_PIN, STEP_PIN, CS_PIN);
+
+void setup() {
+	Serial.begin(115200);
+	while(!Serial);
+	Serial.println("Start...");
+  Serial.print("Testing driver: ");
+  Serial.println(TEST_STEPPER);
+  
+	SPI.begin();
+	pinMode(MISO, INPUT_PULLUP);
+	driver.begin(); 			// Initiate pins and registeries
+	driver.rms_current(300); 	// Set stepper current to 600mA. The command is the same as command TMC2130.setCurrent(600, 0.11, 0.5);
+	driver.stealthChop(1); 	// Enable extremely quiet stepping
+	
+	digitalWrite(EN_PIN, LOW);
+}
+
+void loop() {
+	digitalWrite(STEP_PIN, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(STEP_PIN, LOW);
+	delayMicroseconds(10);
+	uint32_t ms = millis();
+	static uint32_t last_time = 0;
+	if ((ms - last_time) > 2000) {
+		if (dir) {
+			Serial.println("Dir -> 0");
+			driver.shaft_dir(0);
+		} else {
+			Serial.println("Dir -> 1");
+			driver.shaft_dir(1);
+		}
+		dir = !dir;
+		last_time = ms;
+	}
+}
